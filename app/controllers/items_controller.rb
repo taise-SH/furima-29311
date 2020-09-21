73fd5394ecd32adb021e1eee2fce8a628f28d425
taise-SH <69847025+taise-SH@users.noreply.github.com>
2020-09-21 22:12:59 +0900

    ビューの書き換え

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,20 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: :index
 
-
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
@@ -13,4 +25,10 @@ class ItemsController < ApplicationController
     end
   end
   
+  def item_params
+    params.require(:item).permit(:name, :explanation, :image, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
 end
+
+
