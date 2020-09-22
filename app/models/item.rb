a39b66e793bd89c7d491503d1410ba87ad5e2c11
taise-SH <69847025+taise-SH@users.noreply.github.com>
2020-09-22 10:58:32 +0900

    アソシエーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one    :purchase
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :postage_payer
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :delivery_day
+  belongs_to_active_hash :prefecture
 
   validates :name, :explanation, :image, :price ,presence: true
   validates :category_id, :condition_id, :postage_payer_id, :prefecture_id, :delivery_day_id, numericality: { other_than: 0 }
