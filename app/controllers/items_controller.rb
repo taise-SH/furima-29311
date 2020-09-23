e68412ad934b1af39fabeb4f2d0eeab4104b7c21
taise-SH <69847025+taise-SH@users.noreply.github.com>
2020-09-23 20:01:00 +0900

    商品の詳細編集機能の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,11 +23,20 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
 
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   def delete
-   
   end
 
   
