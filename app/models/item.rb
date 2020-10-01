40a37ec4e76810c9d441cd62f8f2b4a4d875a74e
taise-SH <69847025+taise-SH@users.noreply.github.com>
2020-10-01 16:48:05 +0900

    エラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_one :order
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
@@ -9,7 +10,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_day
   belongs_to_active_hash :prefecture
 
-  validates :name, :explanation, :image, :price ,presence: true
-  validates :category_id, :condition_id, :postage_payer_id, :prefecture_id, :delivery_day_id, numericality: { other_than: 0 }
-  validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :name, :explanation, :image, :price, presence: { message: "can't be blank" }
+  validates :category_id, :condition_id, :postage_payer_id, :prefecture_id, :delivery_day_id, numericality: { other_than: 0, message: "can't be blank"}
+  validates :price, format: { with: /\A[0-9]+\z/, message: "is not a number"}, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "must be greater than or equal to 333"}
 end
